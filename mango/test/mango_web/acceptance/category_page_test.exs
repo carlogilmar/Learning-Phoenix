bf604c42c196afffc6d10b64c3aa4f0a128c257c
carlogilmar <carlogilmar12@gmail.com>
2018-07-15 18:52:04 -0500

    #2 Pass acceptance test for show the controller

--- mango/test/mango_web/acceptance/category_page_test.exs
+++ mango/test/mango_web/acceptance/category_page_test.exs
@@ -11,16 +11,13 @@ defmodule MangoWeb.Acceptance.CategoryPageTest do
     # Then
     page_title = find_element(:css, ".page-title")
 							   |> visible_text()
-    assert page_title == "Fruits"
+    assert page_title == "fruits"
     # And
-    product = find_element(:css, ".product")
-							|> visible_text()
-    product_name = find_within_element( product, :css, ".product-name")
-							     |> visible_text()
-    product_price = find_within_element( product, :css, ".product-price")
-                    |> visible_text()
-    assert product_name == "Apple"
-    assert product_price == "100"
+     product = find_element(:css, ".product")
+     product_name = find_within_element(product, :css, ".product-name") |> visible_text()
+     product_price = find_within_element(product, :css, ".product-price") |> visible_text()
+     assert product_name == "Apple"
+     assert product_price == "100"
   end
 
   # test "show vegetables" do
