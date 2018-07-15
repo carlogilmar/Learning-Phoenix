72ec0018eecb8200d06af048858fda172509e59f
carlogilmar <carlogilmar12@gmail.com>
2018-07-15 03:13:47 -0500

    #1 Mango: Modify home page acceptance test

--- mango/test/mango_web/acceptance/homepage_test.exs
+++ mango/test/mango_web/acceptance/homepage_test.exs
@@ -1,19 +1,27 @@
 defmodule MangoWeb.Acceptance.HomepageTest do
 
-  use ExUnit.Case
-  use Hound.Helpers
+	use ExUnit.Case
+	use Hound.Helpers
 
-  hound_session()
+	hound_session()
 
-  test "Presence of seasonal products" do
-    assert page_source() =~ "Seasonal products"
-    # Given: I start the main flow
-    # When: I show the main page
-    navigate_to("/")
-    # Then: I should get the seasonal products
-    page_title = find_element(:id, "title")
-                  |> visible_text()
-    assert page_title == "Seasonal Products"
-  end
+	test "Presence of seasonal products" do
+		# Given: I start the main flow
+		# When: I show the main page
+		navigate_to("/")
+
+		# Then: I should get the seasonal products
+		page_title = find_element(:id, "title")
+							 |> visible_text()
+		assert page_title == "Seasonal Products"
+
+		# And
+		product = find_element(:css, ".product")
+		product_name = find_within_element(product, :css, ".product-name") |> visible_text()
+		product_price = find_within_element(product, :css, ".product-price") |> visible_text()
+		assert product_name == "Apple"
+		assert product_price == "100"
+		refute page_source() =~ "Tomato"
+	end
 
 end
