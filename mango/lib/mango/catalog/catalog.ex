e5ecbcd8c0bf6c1fab53327bf9d80d5cf7006208
carlogilmar <carlogilmar12@gmail.com>
2018-07-15 15:43:18 -0500

    #1 Mango: Micro refactoring

--- mango/lib/mango/catalog/catalog.ex
+++ mango/lib/mango/catalog/catalog.ex
@@ -2,13 +2,13 @@ defmodule Mango.Catalog do
 
   alias Mango.Catalog.Product
 
-  def list_products do
+  def list_products() do
     product1 = %Product{ name: "Tomato", price: 50, is_seasonal: true}
     product2 = %Product{ name: "Apple", price: 100, is_seasonal: false}
     [product1, product2]
   end
 
-  def list_seasonal_products do
+  def list_seasonal_products() do
     list_products
     |> Enum.filter( fn(product) -> product.is_seasonal == true end )
   end
