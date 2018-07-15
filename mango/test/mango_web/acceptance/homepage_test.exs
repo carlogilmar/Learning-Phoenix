5c27cee40d1270ba89834f767b98774e3b4b254a
carlogilmar <carlogilmar12@gmail.com>
2018-07-15 02:34:55 -0500

    Mango: Passing simple acceptance test

--- mango/test/mango_web/acceptance/homepage_test.exs
+++ mango/test/mango_web/acceptance/homepage_test.exs
@@ -5,9 +5,15 @@ defmodule MangoWeb.Acceptance.HomepageTest do
 
   hound_session()
 
-  test "Presence of featured products" do
-    navigate_to("/")
+  test "Presence of seasonal products" do
     assert page_source() =~ "Seasonal products"
+    # Given: I start the main flow
+    # When: I show the main page
+    navigate_to("/")
+    # Then: I should get the seasonal products
+    page_title = find_element(:id, "title")
+                  |> visible_text()
+    assert page_title == "Seasonal Products"
   end
 
 end
