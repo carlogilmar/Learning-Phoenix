865ec8a4736c2de5d6fa79621eb0f68a2a628fe8
carlogilmar <carlogilmar12@gmail.com>
2018-07-14 23:55:50 -0500

    Adding pattern matching example

---
+++ pattern_matching.exs
@@ -0,0 +1,19 @@
+defmodule Sampler do
+
+  def hello() do
+    "Cállate estúpido!!"
+  end
+
+  def get_first(number) do
+    number + 100
+  end
+
+  def get_second(number) do
+    number + 999
+  end
+
+  def get_sum( a, b) do
+    a + b
+  end
+
+end
