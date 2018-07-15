952a635113089b109e4531f515cb4a7b14cfc319
carlogilmar <carlogilmar12@gmail.com>
2018-07-15 03:20:55 -0500

    Mango: Add new context and a struct for pass the catalog_test

---
+++ mango/lib/mango/catalog/product.ex
@@ -0,0 +1,3 @@
+defmodule Mango.Catalog.Product do
+  defstruct [:name, :price]
+end
--- mango/test/mango/catalog/catalog_test.exs
+++ mango/test/mango/catalog/catalog_test.exs
@@ -2,11 +2,12 @@ defmodule Mango.CatalogTest do
 
   use ExUnit.Case
   alias Mango.Catalog
+  alias Mango.Catalog.Product
 
   test "List_products returns all products" do
-    [p1, p2] = Catalog.list_products
-    assert p1 == "Tomato"
-    assert p2 == "Apple"
+    [p1 = %Product{}, p2 = %Product{}] = Catalog.list_products
+    assert p1.name == "Tomato"
+    assert p2.name == "Apple"
   end
 
 end
