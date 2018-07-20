cdb58afaea6fec2351e2efd577e96cc587deb134
carlogilmar <carlogilmar12@gmail.com>
2018-07-20 00:12:43 -0500

    #2 Adding ecto and repo

--- mango/lib/mango/catalog/catalog.ex
+++ mango/lib/mango/catalog/catalog.ex
@@ -1,11 +1,13 @@
 defmodule Mango.Catalog do
 
   alias Mango.Catalog.Product
+  alias Mango.Repo
 
   def list_products() do
-    product1 = %Product{ name: "Tomato", price: 50, category: "vegetables", is_seasonal: true}
-    product2 = %Product{ name: "Apple", price: 100, category: "fruits", is_seasonal: false}
-    [product1, product2]
+    #product1 = %Product{ name: "Tomato", price: 50, category: "vegetables", is_seasonal: true}
+    #product2 = %Product{ name: "Apple", price: 100, category: "fruits", is_seasonal: false}
+    #[product1, product2]
+    Product |> Repo.all
   end
 
   def list_seasonal_products() do
