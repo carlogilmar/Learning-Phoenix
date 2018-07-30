66ec2b892a83cca16924bc289071eae0da75c9b4
carlogilmar <carlogilmar12@gmail.com>
2018-07-30 00:37:28 -0500

    Adding a csv file and a seed script

--- mango/priv/repo/seeds.exs
+++ mango/priv/repo/seeds.exs
@@ -1,4 +1,14 @@
-alias Mango.Repo
+alias NimbleCSV.RFC4180, as: CSV
 alias Mango.Catalog.Product
-Repo.insert %Product{name: "Tomato", price: 50, is_seasonal: false, category: "vegetables"}
-Repo.insert %Product{name: "Apple", price: 100, is_seasonal: true, category: "fruits"}
+alias Mango.Repo
+
+"priv/seed_data/product_list.csv"
+|> File.read!
+|> CSV.parse_string
+|> Enum.each(fn [_, name, price, sku, is_seasonal, image, pack_size, category] ->
+     is_seasonal = String.to_existing_atom(is_seasonal)
+     price = Decimal.new(price)
+     %Product{name: name, price: price, sku: sku, is_seasonal: is_seasonal, image: image, pack_size: pack_size, category: category}
+     |> Repo.insert
+end)
+
