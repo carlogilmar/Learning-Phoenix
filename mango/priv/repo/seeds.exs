54c9e44e039635743f7ec3130722a8d0c9e34e4e
carlogilmar <carlogilmar12@gmail.com>
2018-07-30 00:29:41 -0500

    Seed data

--- mango/priv/repo/seeds.exs
+++ mango/priv/repo/seeds.exs
@@ -1,11 +1,4 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Mango.Repo.insert!(%Mango.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Mango.Repo
+alias Mango.Catalog.Product
+Repo.insert %Product{name: "Tomato", price: 50, is_seasonal: false, category: "vegetables"}
+Repo.insert %Product{name: "Apple", price: 100, is_seasonal: true, category: "fruits"}
