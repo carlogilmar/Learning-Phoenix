a1765c97fd08a22b129a94ce77d0987a24ed88bb
carlogilmar <carlogilmar12@gmail.com>
2018-08-23 01:50:04 -0500

    Adding my first gen server

--- password_lock/lib/password_lock.ex
+++ password_lock/lib/password_lock.ex
@@ -1,18 +1,24 @@
 defmodule PasswordLock do
-  @moduledoc """
-  Documentation for PasswordLock.
-  """
 
-  @doc """
-  Hello world.
+  use GenServer #GenServer is a behavior module that will force you to implement certain definitions inside your module
 
-  ## Examples
+  # Client API
 
-      iex> PasswordLock.hello
-      :world
-
-  """
-  def hello do
-    :world
+  # Starting...
+  def statr_link( password ) do
+    GenServer.start_link(__MODULE__, password, []) # This returns a pid for make request!
   end
+
+  # Functions for gen server
+  def unlock(server_pid, password) do
+    # Call a gen server by server_pid
+    GenServer.call(server_pid, {:unlock, password})
+  end
+
+  def reset(server_pid, {old_password,new_password}) do
+    # Call a gen server by server_pid
+    GenServer.call(server_pid, {:reset, {old_password,new_password}})
+  end
+
+
 end
