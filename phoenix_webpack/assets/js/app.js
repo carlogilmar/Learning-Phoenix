55c46e721b7cc77768907977dd8e00811ff47527
carlogilmar <carlogilmar12@gmail.com>
2018-11-03 20:31:13 -0600

    Adding webpack configuration

---
+++ phoenix_webpack/assets/js/app.js
@@ -0,0 +1,12 @@
+import css from "../css/app.css"
+
+import "phoenix_html"
+
+// Import local files
+//
+// Local files can be imported directly using relative
+// paths "./socket" or full ones "web/static/js/socket".
+
+// import socket from "./socket"
+
+console.log("Que onda aquí andamos!!");
