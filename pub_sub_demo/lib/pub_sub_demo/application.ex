172d1b5f9b4ec90fef1e5ef2b73d0ef8c84cda7d
Carlo Gilmar <carlogilmar12@gmail.com>
2019-07-17 19:31:32 -0500

    Adding pubsub as children of the supervisor

--- pub_sub_demo/lib/pub_sub_demo/application.ex
+++ pub_sub_demo/lib/pub_sub_demo/application.ex
@@ -1,19 +1,16 @@
 defmodule PubSubDemo.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
     children = [
-      # Starts a worker by calling: PubSubDemo.Worker.start_link(arg)
-      # {PubSubDemo.Worker, arg}
+      %{
+        id: Phoenix.PubSub.PG2,
+        start: {Phoenix.PubSub.PG2, :start_link, [:fridge, []]}
+      }
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: PubSubDemo.Supervisor]
     Supervisor.start_link(children, opts)
   end
