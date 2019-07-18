f62a9798d4d7ec742a5c63c3615dfda73595f66f
Carlo Gilmar <carlogilmar12@gmail.com>
2019-07-17 19:25:58 -0500

    Adding pubsub concept project

---
+++ pub_sub_demo/lib/pub_sub_demo.ex
@@ -0,0 +1,18 @@
+defmodule PubSubDemo do
+  @moduledoc """
+  Documentation for PubSubDemo.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PubSubDemo.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
