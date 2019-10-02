fedc38b8d304aa01a8bc7a44b53104938191936c
Carlo Gilmar <carlogilmar12@gmail.com>
2019-10-01 22:42:39 -0500

    Adding correct deps

--- guardian_toy/mix.exs
+++ guardian_toy/mix.exs
@@ -43,8 +43,8 @@ defmodule GuardianToy.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:comeonin, "~> 4.0"},
-      {:argon2_elixir, "~> 1.3"},
+      {:comeonin, "~> 5.1"},
+      {:argon2_elixir, "~> 2.0"},
       {:guardian, "~> 1.2"}
     ]
   end
