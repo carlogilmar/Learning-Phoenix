9c7c9b413ad66058d12dada7c39ba18ce9d97bb0
Carlo Gilmar <carlogilmar12@gmail.com>
2019-10-14 13:50:08 -0500

    Adding changes

--- pub_sub_demo/lib/pub_sub_demo/fridge.ex
+++ pub_sub_demo/lib/pub_sub_demo/fridge.ex
@@ -2,12 +2,12 @@ defmodule PubSubDemo.Fridge do
   @pubsub_name :fridge
   @pubsub_topic "fridge_updates"
 
-  def take(product, quantity) when is_binary(product) and is_integer(quantity) do
+  def take(product, quantity) do
     IO.puts "Fridge in action! Take"
     Phoenix.PubSub.broadcast(@pubsub_name, @pubsub_topic, {:take, product, quantity})
   end
 
-  def return(product, quantity) when is_binary(product) and is_integer(quantity) do
+  def return(product, quantity) do
     IO.puts "Fridge in action! Return"
     Phoenix.PubSub.broadcast(@pubsub_name, @pubsub_topic, {:return, product, quantity})
   end
